d07fdeb26a37a49f1c6426d4f0976ac35efa6bd1
Teddy Guerrand <teddyguerrand76@gmail.com>
2022-08-01 14:44:37 +0200

    Ajout du code test maintien de la session avec cookie

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+const sessions = require('express-session');
+const cookieParser = require("cookie-parser");
 
 const saucesRoutes = require('./routes/sauce')
 const userRoutes = require('./routes/user');
@@ -16,6 +18,52 @@ mongoose.connect(process.env.MONGO_URI,
 
 
 const app = express();
+
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: process.env.TOKEN,
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false 
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname));
+
+app.use(cookieParser());
+
+const myusername = 'user1'
+const mypassword = 'mypassword'
+
+// a variable to save a session
+let session;
+
+app.get('/',(req,res) => {
+  session=req.session;
+  if(session.userid){
+      res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+  }else
+  res.sendFile('views/index.html',{root:__dirname})
+});
+
+app.post('/user',(req,res) => {
+  if(req.body.username == myusername && req.body.password == mypassword){
+      session=req.session;
+      session.userid=req.body.username;
+      console.log(req.session)
+      res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
+  }
+  else{
+      res.send('Invalid username or password');
+  }
+})
+
+app.get('/logout',(req,res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
